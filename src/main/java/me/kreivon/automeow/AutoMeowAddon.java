64e67d38c9088ceb6effdbd6b45cc6ee450221b1
Kreivon <kreivon@proton.me>
2023-12-03 13:58:29 -0500

    Update to use Meteor event over Fabric API

--- src/main/java/me/kreivon/automeow/AutoMeowAddon.java
+++ src/main/java/me/kreivon/automeow/AutoMeowAddon.java
@@ -3,10 +3,9 @@ package me.kreivon.automeow;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import org.slf4j.Logger;
 
-public class AutoMeow extends MeteorAddon {
+public class AutoMeowAddon extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
 
     @Override
@@ -15,8 +14,6 @@ public class AutoMeow extends MeteorAddon {
 
         // Modules
         Modules.get().add(new me.kreivon.automeow.modules.AutoMeow());
-        ClientReceiveMessageEvents.ALLOW_CHAT.register(new me.kreivon.automeow.modules.AutoMeow.AutoMeowChatEvent());
-        ClientReceiveMessageEvents.MODIFY_GAME.register(new me.kreivon.automeow.modules.AutoMeow.AutoMeowGameEvent());
     }
 
     @Override
--- src/main/java/me/kreivon/automeow/modules/AutoMeow.java
+++ src/main/java/me/kreivon/automeow/modules/AutoMeow.java
@@ -1,6 +1,6 @@
 package me.kreivon.automeow.modules;
 
-import com.mojang.authlib.GameProfile;
+import meteordevelopment.meteorclient.events.game.ReceiveMessageEvent;
 import meteordevelopment.meteorclient.settings.IntSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
@@ -8,15 +8,8 @@ import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.network.message.MessageType;
-import net.minecraft.network.message.SignedMessage;
-import net.minecraft.text.Text;
+import meteordevelopment.orbit.EventHandler;
 import net.minecraft.util.Util;
-import org.jetbrains.annotations.Nullable;
-
-import java.time.Instant;
 
 public class AutoMeow extends Module {
 
@@ -43,37 +36,13 @@ public class AutoMeow extends Module {
         lastMeow = Util.getMeasuringTimeMs() + cooldown.get();
     }
 
-    public static class AutoMeowChatEvent implements ClientReceiveMessageEvents.AllowChat {
-
-        @Override
-        public boolean allowReceiveChatMessage(Text message, @Nullable SignedMessage signedMessage, @Nullable GameProfile sender, MessageType.Parameters params, Instant receptionTimestamp) {
-            if (Modules.get().get("Auto Meow").isActive()) {
-                if (MinecraftClient.getInstance().player != null && message.getString().toUpperCase().contains("MEOW")) {
-                    if (sender != null && sender.getId() == MinecraftClient.getInstance().player.getUuid())
-                        return true;
-                    if (Util.getMeasuringTimeMs() - lastMeow >= (int) Modules.get().get("Auto Meow").settings.get("Cooldown (ms)").get()) {
-                        lastMeow = Util.getMeasuringTimeMs();
-                        ChatUtils.sendPlayerMsg("meow :3");
-                    }
-                }
+    @EventHandler
+    private void onMessageReceive(ReceiveMessageEvent event) {
+        if (mc.player != null && event.getMessage().getString().toUpperCase().contains("MEOW") && !event.getMessage().contains(mc.player.getName())) {
+            if (Util.getMeasuringTimeMs() - lastMeow >= (int) Modules.get().get("Auto Meow").settings.get("Cooldown (ms)").get()) {
+                lastMeow = Util.getMeasuringTimeMs();
+                ChatUtils.sendPlayerMsg("meow :3");
             }
-            return true;
-        }
-    }
-
-    public static class AutoMeowGameEvent implements ClientReceiveMessageEvents.ModifyGame {
-
-        @Override
-        public Text modifyReceivedGameMessage(Text message, boolean overlay) {
-            if (Modules.get().get("Auto Meow").isActive()) {
-                if (MinecraftClient.getInstance().player != null && message.getString().toUpperCase().contains("MEOW") && !message.contains(MinecraftClient.getInstance().player.getName())) {
-                    if (Util.getMeasuringTimeMs() - lastMeow >= (int) Modules.get().get("Auto Meow").settings.get("Cooldown (ms)").get()) {
-                        lastMeow = Util.getMeasuringTimeMs();
-                        ChatUtils.sendPlayerMsg("meow :3");
-                    }
-                }
-            }
-            return message;
         }
     }
 
